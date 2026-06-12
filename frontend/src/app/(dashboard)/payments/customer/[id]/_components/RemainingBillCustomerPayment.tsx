a70d5299eb21942686407616c7644fcf461fce6e
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-06-12 17:25:49 +0700

    style : adjust color button remaining bill adaptive by percentagePayment

--- frontend/src/app/(dashboard)/payments/customer/[id]/_components/RemainingBillCustomerPayment.tsx
+++ frontend/src/app/(dashboard)/payments/customer/[id]/_components/RemainingBillCustomerPayment.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { CustomerPayment } from "@/types/payment";
 import { formatRupiah } from "@/utils/helper";
-import { toast } from "sonner";
 import { CreditCard } from "lucide-react";
 import { BaseModal } from "@/components/ui/modal/BaseModal";
 import { CustomerPaymentForm } from "@/components/ui/form/CustomerPaymentForm";
@@ -37,14 +36,16 @@ export default function RemainingBillCustomerPayment({
         <h3 className="text-xs font-semibold text-zinc-400">Remaining Bills</h3>
       </div>
 
-      <div className="text-2xl font-black text-red-500">
+      <div
+        className={`text-2xl font-black ${paymentPercentage < 50 ? "text-red-500" : paymentPercentage < 95 ? "text-yellow-500" : "text-emerald-500"}`}
+      >
         {formatRupiah(payment.customer?.remaining_bill || 0)}
       </div>
 
       <div className="space-y-1">
         <div className="h-2 w-full bg-zinc-100 rounded-full overflow-hidden">
           <div
-            className="h-full text-primary-brand rounded-full transition-all duration-500"
+            className={`h-full ${paymentPercentage < 50 ? "bg-red-500" : paymentPercentage < 95 ? "bg-yellow-500" : "bg-emerald-500"} rounded-full transition-all duration-500`}
             style={{ width: `${paymentPercentage}%` }}
           />
         </div>
@@ -56,7 +57,7 @@ export default function RemainingBillCustomerPayment({
 
       <button
         onClick={() => setIsModalOpen(true)}
-        className={`w-full flex items-center justify-center gap-2 py-2.5 text-xs font-bold text-primary-brand bg-blue-50 hover:bg-blue-100 border border-blue-200 rounded-xl transition-all ${payment.customer.remaining_bill === 0 ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}
+        className={`w-full flex items-center justify-center gap-2 py-2.5 text-xs font-bold  ${paymentPercentage < 50 ? "bg-red-50 hover:bg-red-100 border border-red-200 text-red-500 " : paymentPercentage < 95 ? "bg-yellow-50 hover:bg-yellow-100 border border-yellow-200 text-yellow-500" : "bg-emerald-50 hover:bg-emerald-100 border border-emerald-200 text-emerald-500"} rounded-xl transition-all ${payment.customer.remaining_bill === 0 ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}
         disabled={payment.customer.remaining_bill === 0}
       >
         <CreditCard className="w-4 h-4" /> Record Remaining Payment
--- frontend/src/app/(dashboard)/payments/supplier/[id]/_components/RemainingBillSupplierPayment.tsx
+++ frontend/src/app/(dashboard)/payments/supplier/[id]/_components/RemainingBillSupplierPayment.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { SupplierPayment } from "@/types/payment";
 import { formatRupiah } from "@/utils/helper";
-import { toast } from "sonner";
 import { CreditCard } from "lucide-react";
 import { BaseModal } from "@/components/ui/modal/BaseModal";
 import { SupplierPaymentForm } from "@/components/ui/form/SupplierPaymentForm";
@@ -39,14 +38,16 @@ export default function RemainingBillSupplierPayment({
         </h3>
       </div>
 
-      <div className="text-2xl font-black text-red-500">
+      <div
+        className={`text-2xl font-black ${paymentPercentage < 50 ? "text-red-500" : paymentPercentage < 95 ? "text-yellow-500" : "text-emerald-500"}`}
+      >
         {formatRupiah(payment.supplier?.remaining_bill || 0)}
       </div>
 
       <div className="space-y-1">
         <div className="h-2 w-full bg-zinc-100 rounded-full overflow-hidden">
           <div
-            className="h-full bg-emerald-500 rounded-full transition-all duration-500"
+            className={`h-full ${paymentPercentage < 50 ? "bg-red-500" : paymentPercentage < 95 ? "bg-yellow-500" : "bg-emerald-500"} rounded-full transition-all duration-500`}
             style={{ width: `${paymentPercentage}%` }}
           />
         </div>
@@ -58,7 +59,7 @@ export default function RemainingBillSupplierPayment({
 
       <button
         onClick={() => setIsModalOpen(true)}
-        className={`w-full flex items-center justify-center gap-2 py-2.5 text-xs font-bold text-emerald-700 bg-emerald-50 hover:bg-emerald-100 border border-emerald-200 rounded-xl transition-all ${payment.supplier.remaining_bill === 0 ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}
+        className={`w-full flex items-center justify-center gap-2 py-2.5 text-xs font-bold  ${paymentPercentage < 50 ? "bg-red-50 hover:bg-red-100 border border-red-200 text-red-500 " : paymentPercentage < 95 ? "bg-yellow-50 hover:bg-yellow-100 border border-yellow-200 text-yellow-500" : "bg-emerald-50 hover:bg-emerald-100 border border-emerald-200 text-emerald-500"} rounded-xl transition-all ${payment.supplier.remaining_bill === 0 ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}
         disabled={payment.supplier.remaining_bill === 0}
       >
         <CreditCard className="w-4 h-4" /> Pay Remaining Balance
