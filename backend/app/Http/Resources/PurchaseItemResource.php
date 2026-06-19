df8d67bba158d63d3f5e2b75a939e0e5a5897a31
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-06-19 22:18:52 +0700

    fix : price items at purchase Edit form

--- backend/app/Http/Resources/PurchaseItemResource.php
+++ backend/app/Http/Resources/PurchaseItemResource.php
@@ -28,7 +28,7 @@ class PurchaseItemResource extends JsonResource
             'price'             => (float) $this->price,
             'subtotal'          => (float) $this->subtotal,
 
-            'cost_price'        => (int) $this->price * $this->ProductUnit->conversion_factor,
+            'cost_price'        => (int) $this->price / $this->quantity,
             'remaining_qty'     => (int) $this->remaining_qty,
             'batch_status'      => $this->remaining_qty <= 0 ? 'habis' : 'tersedia',
             'product'           => new ProductResource($this->whenLoaded('Product')),
